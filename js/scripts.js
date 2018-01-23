2bc038802dae52ce93ce8a9dbc1d40a588540a56
Katarzyna Skotnicka <ka.skotnicka@gmail.com>
2018-01-23 19:53:46 +0100

    Add carousel movement

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
+//carousel movement
+
+var carouselList = $(".slides");
+
+function changeSlide() {
+	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+}
+
+function moveFirstSlide() {
+	var firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");
+
+	lastItem.after(firstItem);
+	carouselList.css({marginLeft:0});
+}
+
 $(function() {
-
-	var carouselList = $(".slides");
-
-	setInterval(changeSlide() {
-
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide() {
-			var firstItem = carouselList.find("li:first");
-			var lastItem = carouselList.find("li:last");
-			lastItem.after(firstItem);
-			carouselList.css({marginLeft:0});
-		}
-	);
-
-	}, 3000);
-
+	setInterval(changeSlide, 3000);
 });
+
+
+// carousel controls right & left
+
+var arrowLeft = $(".glyphicon-menu-left"),
+	arrowRight = $("glyphicon-menu-right");
+
+
+function goToNextSlide() {
+	carouselList.animate({'marginLeft':800}, 500);
+	}
+
+function goToPreviousSlide() {
+	carouselList.animate({'marginRight':800}, 500);
+}
+
+
+$(arrowLeft).on("click", goToNextSlide);
+$(arrowRight).on("click", goToPreviousSlide);
